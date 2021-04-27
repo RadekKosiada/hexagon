f383695616db5aa788bca68998b2b0eaba4e109e
RadekKosiada <kosiadar@gmail.com>
2021-04-27 20:57:37 +0200

    Rebuild server to send sample data

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,9 @@ const express = require("express");
 const app = express();
 const port = 3001;
 
-app.get("/", function(request, response) {
-  response.send("Express is working");
+app.get("/data", function(request, response) {
+  const data = [{ id: 0}]
+  response.json(data);
 })
 
 app.listen(port, () => {
