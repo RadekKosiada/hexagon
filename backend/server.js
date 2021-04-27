4524decdd43bebd4fd0e040cca4705249b9e96fd
RadekKosiada <kosiadar@gmail.com>
2021-04-27 20:03:17 +0200

    Add nodemon script & add port as variable

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const app = express();
+const port = 3001;
 
 app.get("/", function(request, response) {
   response.send("Express is working");
 })
 
-app.listen(3001, () => {
-  console.log("Express server is running");
+app.listen(port, () => {
+  console.log(`Express server is running on ${port}`);
 });
