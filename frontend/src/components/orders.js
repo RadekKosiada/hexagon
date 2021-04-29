827a2332159bc0274074f591b7d8783a621733eb
RadekKosiada <kosiadar@gmail.com>
2021-04-29 23:46:09 +0200

    Get uid at Orders

--- frontend/src/components/orders.js
+++ frontend/src/components/orders.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import { useAuth } from '../contexts/authContext';
 
 export default function Order() {
+
+  const { currentUser } = useAuth();
+  console.log('user from orders: ', currentUser);
   return (
     <div>
       <button>Logout</button>
+      <p>{currentUser.uid}</p>
       <p>Orders</p>
     </div>
   )
--- frontend/src/contexts/authContext.js
+++ frontend/src/contexts/authContext.js
@@ -26,7 +26,6 @@ export function AuthProvider({children}) {
 
   auth.onAuthStateChanged(user => {
     setCurrentUser(user);
-    console.log('user: ', user);
   });
 
   const value = {
