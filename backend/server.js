e472d559e2c1988af2622db0e570b4592d673f66
RadekKosiada <kosiadar@gmail.com>
2021-05-02 18:09:36 +0200

    Pass user ID to server & display user's orders in frontend

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,32 @@
 const express = require("express");
 const app = express();
 const port = 3001;
+const secrects = require("./secrets");
+const admin = require("firebase-admin");
+const serviceAccount = require("./serviceAccountKey.json");
 
-app.get("/data", function(request, response) {
-  const data = [{ id: 0}]
-  response.json(data);
-})
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: secrects.databaseURL
+});
+
+const auth = admin.auth();
+const db = admin.firestore();
+
+const orders = db.collection("orders");
+
+app.get("/orders:id", async (request, response) => {
+
+  const doc = await orders.doc(request.params.id).get();
+  console.log("User id: ", request.params.id)
+  if (!doc.exists) {
+    console.log("No data to be displayed");
+  } else {
+    console.log("Data: ", doc.data());
+    response.json(doc.data());
+  }
+});
+
 
 app.listen(port, () => {
   console.log(`Express server is running on ${port}`);
--- frontend/src/components/orders.js
+++ frontend/src/components/orders.js
@@ -1,31 +1,54 @@
-import React from 'react';
-import { useAuth } from '../contexts/authContext';
+import React, { useEffect, useState } from "react";
+import { useAuth } from "../contexts/authContext";
 import { useHistory } from "react-router-dom";
 
 export default function Order() {
-
   const { currentUser, logout } = useAuth();
-  console.log('user from orders: ', currentUser);
+  console.log("user from orders: ", currentUser);
 
   const history = useHistory();
+  const [title, setTitle] = useState(["waiting for title"]);
+  const [bookingDate, setBookingDate] = useState(["waiting for booking date"]);
 
   async function handleLogout() {
-    console.log('logout triggered');
+    console.log("logout triggered");
 
     try {
       await logout();
-      history.push('/orders');
+      history.push("/orders");
     } catch {
-      console.log('User cannot be logged out');
+      console.log("User cannot be logged out");
     }
-    
   }
+
+  useEffect(() => {
+    let isMounted = true;
+
+    fetch('/orders' + currentUser.uid)
+      .then(response => {
+        if (response.ok && isMounted) {
+          return response.json();
+        }
+      })
+      .then(jsonResponse => {
+        console.log(jsonResponse.title);
+        console.log(jsonResponse.bookingDate);
+
+        setBookingDate(jsonResponse.bookingDate);
+        setTitle(jsonResponse.title);
+      })
+      .catch(err => console.log(err.message));
+    return () => {
+      isMounted = false;
+    };
+  });
 
   return (
     <div>
       <button onClick={handleLogout}>Logout</button>
       <p>{currentUser.uid}</p>
-      <p>Orders</p>
+      <p>{bookingDate}</p>
+      <p>{title}</p>
     </div>
-  )
+  );
 }
